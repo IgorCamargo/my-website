8c4f3c3f67cd65ddf1114b28900951c7f9686344
Igor Camargo <igorfelcam@hotmail.com>
2019-02-25 12:17:28 -0300

    component scroll

--- src/components/LinkScroll.vue
+++ src/components/LinkScroll.vue
@@ -1,8 +1,11 @@
 <template>
-  <div id="linkscroll">
-    <a href="#work">
-      Trabalhos
-      <div class="">
+  <div id="linkscroll"
+    :style="{ bottom: this.bottom + '%'}"
+  >
+    <a href="#">
+      <div class=""
+        :style="{ transform: 'rotate(' + this.angleComponent + 'deg)' }"
+      >
         <svg version="1.1" id="Capa_1"
           xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
           x="0px" y="0px" width="20px" height="20px"
@@ -23,14 +26,55 @@
 <script>
 export default {
   name: 'LinkScroll',
+  data () {
+    return {
+      lengthPosition: 0,
+      angleComponent: 0,
+      bottom: 50,
+    };
+  },
+  created () {
+    let html = document.documentElement;
+    this.lengthPosition = html.clientHeight;
+  },
+  mounted () {
+    window.addEventListener('scroll', this.handleScroll);
+  },
+  methods: {
+    handleScroll() {
+      let scroll = window.scrollY,
+          percPosition = 0;
+      percPosition = (100*scroll)/this.lengthPosition;
+      this.angleComponent = (percPosition*180)/100;
+
+      // console.log(percPosition);
+      if (percPosition >= 95) {
+
+
+        let y = percPosition; // 95 ~ 100
+
+
+
+        let x = this.bottom - ((100-percPosition)/2);
+
+        console.log(x);
+
+        // this.bottom = 47.5;
+
+
+      }
+    }
+  }
 }
 </script>
 
 <style scoped lang="scss">
 #linkscroll {
   position: absolute;
-  bottom: 50%;
+  // bottom: 50%;
   width: 100%;
+  padding: 5px 0;
   cursor: pointer;
+  z-index: 99;
 }
 </style>
